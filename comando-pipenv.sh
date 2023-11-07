94b7380e29b902eb76bf6fb3bf3defe0dec50dd7
ggchavez-hotmail <69687643+ggchavez-hotmail@users.noreply.github.com>
2023-11-07 12:05:11 -0300

    Create comando-pipenv.sh

---
+++ comando-pipenv.sh
@@ -0,0 +1,12 @@
+#levantar ambiente
+pipenv shell
+
+#instalar paquetes
+pipenv install nombre paquete
+
+#generar los requirimientos
+pipenv requirements > requirements.txt
+
+#salir de ambiente
+deactivate
+
