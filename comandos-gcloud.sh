58322f5193d90427395a8f8e27a54263b74d3cba
ggchavez <gg_chavez@hotmail.com>
2024-03-31 21:01:53 -0300

    comandos

--- comandos-gcloud.sh
+++ comandos-gcloud.sh
@@ -6,3 +6,4 @@ gcloud config set project XX-XXX-XXX-XX
 gcloud components install gke-gcloud-auth-plugin
 #Conectar al Cluster, se debe rescatar de la parte de Kubernetes
 gcloud container clusters get-credentials XXX-XX-XX-XXX-XX --region=us-east4
+gcloud container clusters get-credentials XXX-XX-XX-XXX-XX --region=southamerica-west1-a
