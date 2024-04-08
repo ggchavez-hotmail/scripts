89e89891cd95b7ba81d2a7b75b453a0330faf7d1
ggchavez-hotmail <69687643+ggchavez-hotmail@users.noreply.github.com>
2024-04-08 14:46:47 -0400

    Update comandos-gcloud.sh

--- comandos-gcloud.sh
+++ comandos-gcloud.sh
@@ -7,3 +7,13 @@ gcloud components install gke-gcloud-auth-plugin
 #Conectar al Cluster, se debe rescatar de la parte de Kubernetes
 gcloud container clusters get-credentials XXX-XX-XX-XXX-XX --region=us-east4
 gcloud container clusters get-credentials XXX-XX-XX-XXX-XX --region=southamerica-west1-a
+
+#loggin de los componentes
+gcloud logging logs list
+
+#ver ejecución clourun
+gcloud run jobs executions list --job XXXXXX
+
+#borrar log especifico
+gcloud run jobs executions delete XXXXXX-XXX --quiet
+
